891301cc578f16c6e2b79a93355ff9f575494391
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-04 16:51:56 +0100

    Design awfull (as usual), but it works :D

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark & Tight Concrete Flat Paris',
+  address: '10 rue du Penitancier Paris',
+  description: 'A decrepid old appartement, situated in the catacombs of the building, extremly expensive but thats what you get in Paris',
+  price_per_night: 750,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Dark & Tight Concrete Flat Paris',
+  address: '10 rue du Penitancier Paris',
+  description: 'A decrepid old appartement, situated in the catacombs of the building, extremly expensive but thats what you get in Paris',
+  price_per_night: 750,
+  number_of_guests: 1
+)
